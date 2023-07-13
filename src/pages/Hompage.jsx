e33bea4936e3a48d5539a54987ceb4e736f53941
Guy Kadosh <guyk50@gmail.com>
2023-07-13 09:53:59 +0300

    setting PageNav

--- src/pages/Hompage.jsx
+++ src/pages/Hompage.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom'
+import PageNav from '../components/PageNav'
+
 function Hompage() {
-  return <div>Worldwise</div>
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+    </div>
+  )
 }
 
 export default Hompage
